eb563e86ceafb480a3991cd53078714c05c4ae9e
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-07-17 01:51:48 +0600

    Update New Page UI

--- src/whiteboardApp/HomepageController.java
+++ src/whiteboardApp/HomepageController.java
@@ -3,7 +3,6 @@ package whiteboardApp;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class HomepageController extends Main {
     public void onNewButtonClicked() throws Exception {
