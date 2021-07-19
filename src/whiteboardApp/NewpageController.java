08602eaf6c137b80a9dd025383d17da34a93e52d
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-07-19 13:47:24 +0600

    Add Canvas

--- src/whiteboardApp/NewpageController.java
+++ src/whiteboardApp/NewpageController.java
@@ -1,4 +1,38 @@
 package whiteboardApp;
 
-public class NewpageController {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ColorPicker;
+import javafx.stage.Stage;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NewpageController implements Initializable {
+
+    @FXML
+    public ColorPicker colorPicker;
+    @FXML
+    public Canvas canvas;
+
+    GraphicsContext brushTool;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        brushTool = canvas.getGraphicsContext2D();
+
+        canvas.setOnMouseDragged(draw -> {
+            double size = 20;
+            double x = draw.getX() - size / 2;
+            double y = draw.getY() - size / 2;
+
+            brushTool.setFill(colorPicker.getValue());
+            brushTool.fillRoundRect(x, y, size, size, size, size);
+        });
+    }
 }
