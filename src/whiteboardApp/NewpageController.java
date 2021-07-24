6b89d8ae706e2f6ec464d61eae08a3e4cae6551f
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-07-25 00:54:29 +0600

    Add Pen Tool and Eraser Tool

--- src/whiteboardApp/NewpageController.java
+++ src/whiteboardApp/NewpageController.java
@@ -1,5 +1,6 @@
 package whiteboardApp;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
@@ -8,6 +9,8 @@ import javafx.scene.control.ColorPicker;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -23,24 +26,31 @@ public class NewpageController implements Initializable {
     @FXML
     public MenuItem penTool, eraserTool;
 
-    private void toolSelected(String _tool) {
+    private void useTool(MouseEvent mouseEvent, String _tool) {
         GraphicsContext brushTool = canvas.getGraphicsContext2D();
+        MouseButton mouseButton = mouseEvent.getButton();
+        double size = (toolSize.getText().isEmpty() ? 20 : Double.parseDouble(toolSize.getText()));
+        double x = mouseEvent.getX() - size / 2;
+        double y = mouseEvent.getY() - size / 2;
+        if (mouseButton == MouseButton.PRIMARY) {
+            brushTool.setFill(_tool.equals("Pen") ? colorPicker.getValue() : canvas.getScene().getFill());
+            brushTool.fillRoundRect(x, y, size, size, size, size);
+        }
+    }
+
+    private void toolSelected(String _tool) {
         tool.setText(_tool);
-
-        canvas.setOnMouseClicked(draw -> {
-            double size = (toolSize.getText().isEmpty() ? 20 : Double.parseDouble(toolSize.getText()));
-            double x = draw.getX() - size / 2;
-            double y = draw.getY() - size / 2;
-            brushTool.setFill(_tool.equals("Pen") ? colorPicker.getValue() : canvas.getScene().getFill());
-            brushTool.fillRoundRect(x, y, size, size, size, size);
+        canvas.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                useTool(mouseEvent, _tool);
+            }
         });
-
-        canvas.setOnMouseDragged(draw -> {
-            double size = (toolSize.getText().isEmpty() ? 20 : Double.parseDouble(toolSize.getText()));
-            double x = draw.getX() - size / 2;
-            double y = draw.getY() - size / 2;
-            brushTool.setFill(_tool.equals("Pen") ? colorPicker.getValue() : canvas.getScene().getFill());
-            brushTool.fillRoundRect(x, y, size, size, size, size);
+        canvas.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                useTool(mouseEvent, _tool);
+            }
         });
     }
 
