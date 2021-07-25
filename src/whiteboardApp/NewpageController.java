afe3faa091ee81600064293abcb0538bd2064f3f
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-07-25 23:33:23 +0600

    Fix Pen Tool and Eraser Tool Bug

--- src/whiteboardApp/NewpageController.java
+++ src/whiteboardApp/NewpageController.java
@@ -1,75 +1,57 @@
 package whiteboardApp;
 
-import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class NewpageController implements Initializable {
     @FXML
-    public Button backButton, exportButton;
+    public Button backButton, exportButton, sizeButton;
     @FXML
     public ColorPicker colorPicker;
     @FXML
     public Canvas canvas;
     @FXML
-    public TextField toolSize;
-    @FXML
     public MenuButton tool;
     @FXML
     public MenuItem penTool, eraserTool;
-
-    private void useTool(MouseEvent mouseEvent, String _tool, char mouseAction) {
-        GraphicsContext brushTool = canvas.getGraphicsContext2D();
-        double size = (toolSize.getText().isEmpty() ? 10 : Double.parseDouble(toolSize.getText()));
-        double x = mouseEvent.getX() - size / 2;
-        double y = mouseEvent.getY() - size / 2;
-        if (mouseEvent.getButton() == MouseButton.PRIMARY) {
-            brushTool.setFill(_tool.equals("Pen") ? colorPicker.getValue() : canvas.getScene().getFill());
-            if (mouseAction == 'p') {
-                brushTool.beginPath();
-                brushTool.moveTo(x, y);
-                brushTool.stroke();
-            } else if (mouseAction == 'd') {
-                brushTool.lineTo(x, y);
-                brushTool.stroke();
-                brushTool.closePath();
-                brushTool.beginPath();
-                brushTool.moveTo(x, y);
-            } else {
-                brushTool.lineTo(x, y);
-                brushTool.stroke();
-                brushTool.closePath();
-            }
-        }
-    }
+    @FXML
+    public ToolBar toolBar;
+    public GraphicsContext canvasTool;
 
     private void toolSelected(String _tool) {
         tool.setText(_tool);
-        canvas.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                useTool(mouseEvent, _tool, 'p');
+        canvasTool = canvas.getGraphicsContext2D();
+        canvasTool.setStroke(_tool == "Pen" ? Color.BLACK : canvas.getScene().getFill());
+        canvasTool.setLineWidth(1);
+        if (_tool == "Pen") {
+            colorPicker.setOnAction(event -> {
+                canvasTool.setStroke(colorPicker.getValue());
+            });
+        }
+        canvas.setOnMousePressed(mouseEvent -> {
+            if (mouseEvent.getButton() == MouseButton.PRIMARY) {
+                canvasTool.beginPath();
+                canvasTool.lineTo(mouseEvent.getX(), mouseEvent.getY());
+                canvasTool.stroke();
             }
         });
-        canvas.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                useTool(mouseEvent, _tool, 'd');
-            }
+        canvas.setOnMouseDragged(mouseEvent ->  {
+                if (mouseEvent.getButton() == MouseButton.PRIMARY) {
+                    canvasTool.lineTo(mouseEvent.getX(), mouseEvent.getY());
+                    canvasTool.stroke();
+                }
         });
-        canvas.setOnMouseReleased(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                useTool(mouseEvent, _tool, 'r');
-            }
-        });
+    }
+
+    public void sizeButtonSelected(ActionEvent actionEvent) {
     }
 
     public void penSelected() {
